41cdedec19bdcd937118420c46442ebb90a5e8a7
Lian Salmerón López <lembajosa@gmail.com>
2021-11-02 19:12:20 +0100

    Adición de Carpetas
    Se han implementado las carpetas de controlador y modelo

---
+++ Pictureka/src/Controlador/ControladorLogo.java
@@ -0,0 +1,17 @@
+package Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControladorLogo {
+
+    @FXML
+    private Button btnLogo;
+
+    @FXML
+    void entrarApp(ActionEvent event) {
+
+    }
+
+}

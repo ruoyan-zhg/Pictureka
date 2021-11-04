b52b0cbbce855916913a52efd1ab74e951501245
Ruoyan Zhang <90801434+ruoyan-zhg@users.noreply.github.com>
2021-11-04 17:02:37 +0100

    controlador login

---
+++ Pictureka/src/Controlador/ControladorLogin.java
@@ -0,0 +1,47 @@
+package Controlador;
+
+import com.gluonhq.charm.glisten.control.Avatar;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+public class ControladorLogin {
+
+    @FXML
+    private ImageView miImageView;
+
+    @FXML
+    private ProgressBar miProgressBar1;
+
+    @FXML
+    private Label miLabel1;
+
+    @FXML
+    private Label miLabel2;
+
+    @FXML
+    private ProgressBar miProgressBar2;
+
+    @FXML
+    private Button miBotton2;
+
+    @FXML
+    private Button miBotton1;
+
+    @FXML
+    private Avatar miAvatar;
+
+    @FXML
+    void IniciarSesion(ActionEvent event) {
+ 
+
+    }
+
+    @FXML
+    void Registrarse(ActionEvent event) {
+
+    }
+
+}

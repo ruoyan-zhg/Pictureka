f0f5130cb798e8417a3bca136acd331b9e615394
Oscar Ramon Gonzalez Guerra <oscargg107@gmail.com>
2021-11-28 23:11:16 +0100

    VentanaGuardia

---
+++ Pictureka/src/Controlador/ControladorGuardia.java
@@ -0,0 +1,99 @@
+package Controlador;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class ControladorGuardia {
+
+    @FXML
+    private AnchorPane anchorPanePrincipal;
+
+    @FXML
+    private VBox VBoxPrincipal;
+
+    @FXML
+    private ImageView imgUsuario;
+
+    @FXML
+    private ImageView imgCerrarSesion;
+
+    @FXML
+    private JFXTextField numTicket;
+
+    @FXML
+    private JFXTextField tituloInforme;
+
+    @FXML
+    private JFXTextField cuerpoInforme;
+
+    @FXML
+    private Text SalaText;
+
+    @FXML
+    private ImageView imgTicket;
+
+    @FXML
+    private ImageView imgCerrarSesion1111;
+
+    @FXML
+    private ButtonBar imgValidar;
+
+    @FXML
+    private ImageView imgEnviarInforme;
+
+    @FXML
+    private ImageView imgFormulario;
+
+    @FXML
+    private ImageView imgSala1;
+
+    @FXML
+    private ImageView imgSala2;
+
+    @FXML
+    private ImageView imgSala4;
+
+    @FXML
+    private ImageView imgSala3;
+
+    @FXML
+    void accederPerfil(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cerrarSesion(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sala1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sala2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sala3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sala4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void validarTicket(MouseEvent event) {
+
+    }
+
+}

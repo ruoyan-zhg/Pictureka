ab52c202c0495779787c6f59ab6aa0c7e7d98009
Oscar Ramon Gonzalez Guerra <oscargg107@gmail.com>
2021-12-09 15:53:23 +0100

    VentanaInformes de administradores

--- Pictureka/src/Controlador/ControladorAdministrador.java
+++ Pictureka/src/Controlador/ControladorAdministrador.java
@@ -1,5 +1,90 @@
 package Controlador;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class ControladorAdministrador {
 
+    @FXML
+    private AnchorPane anchorPanePrincipal;
+
+    @FXML
+    private VBox VBoxPrincipal;
+
+    @FXML
+    private ImageView imgCerrarSesion;
+
+    @FXML
+    private ImageView imgAvatarAdmin;
+
+    @FXML
+    private ImageView imgInformes;
+
+    @FXML
+    private LineChart<?, ?> tablaEntradas;
+
+    @FXML
+    private ImageView imgAniadirUsuario;
+
+    @FXML
+    private ImageView imgEditarUsuario;
+
+    @FXML
+    private ImageView imgTemperatura;
+
+    @FXML
+    private ImageView imgDistancia;
+
+    @FXML
+    private ImageView imgLuz;
+
+    @FXML
+    private Text textSensores;
+
+    @FXML
+    void SensorDistancia(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SensorLuz(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SensorTemperatura(MouseEvent event) {
+
+    }
+
+    @FXML
+    void accederInformes(MouseEvent event) {
+
+    }
+
+    @FXML
+    void accederPerfil(MouseEvent event) {
+
+    }
+
+    @FXML
+    void aniadirUsuario(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cerrarSesion(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editarUsuario(MouseEvent event) {
+
+    }
+
 }
+
---
+++ Pictureka/src/Controlador/ControladorInformeAdmin.java
@@ -0,0 +1,58 @@
+package Controlador;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class ControladorInformeAdmin {
+
+    @FXML
+    private AnchorPane anchorPanePrincipal;
+
+    @FXML
+    private VBox VBoxPrincipal;
+
+    @FXML
+    private ImageView imgUsuario;
+
+    @FXML
+    private ImageView imgCerrarSesion;
+
+    @FXML
+    private JFXTextField mostrarInforme;
+
+    @FXML
+    private TableView<?> tablaInformes;
+
+    @FXML
+    private JFXTextField tituloInforme;
+
+    @FXML
+    private JFXTextField cuerpoInforme;
+
+    @FXML
+    private ImageView imgFormulario;
+
+    @FXML
+    private ImageView imgEnviarInforme;
+
+    @FXML
+    void accederPerfil(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cerrarSesion(MouseEvent event) {
+
+    }
+
+    @FXML
+    void enviarInforme(MouseEvent event) {
+
+    }
+
+}
